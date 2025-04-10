3e05cef4261ce5d0fd074e1ac8f5e95cfc5c0866
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2025-04-10 08:58:35 +0600

    comment

---
+++ 02_comments/multipol_line.php
@@ -0,0 +1,11 @@
+<?php
+
+/*  
+! comment   
+* comment   
+# comment   
+*/
+
+
+
+?>
---
+++ 02_comments/single_line.php
@@ -0,0 +1,16 @@
+<?php
+
+
+// This is comment
+// ! this is comment
+// * this is comment
+// # this is comment
+//  this is comment
+
+
+
+
+
+
+//  localhost/PHP_Start/02_comments/ 
+?>
